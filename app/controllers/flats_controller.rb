24580cb0653efcd2602c21bd87e5423938d1fc5a
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-02-24 17:59:34 +0300

    end of the day

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,43 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Charming English country house',
+  address: '9 Earl\'s Court Canterbury X21 2TR',
+  description: 'Delightful place to spend the spring with family and friends',
+  price_per_night: 95,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Amazing flat with Bosphorus view',
+  address: 'Dr Zafer Tunya Sokak Istanbul 34425',
+  description: 'A spacious apartment with a breathtaking view for unforgettable memories',
+  price_per_night: 200,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Party summer house',
+  address: 'Atatürk Caddesi Fethiye 83392',
+  description: 'Villa by the beach, suited for big parties for you and your friends',
+  price_per_night: 500,
+  number_of_guests: 15
+)
